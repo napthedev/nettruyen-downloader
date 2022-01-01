0752b0f9b11788d5a57de8df52148769c226f6f6
napthedev <phongna.dev@gmail.com>
2022-01-01 10:48:22 +0700

    fetch conversion api when start

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
   import { Router, Route } from "svelte-navigator";
   import Comic from "./pages/Comic.svelte";
   import Home from "./pages/Home.svelte";
+  import { CONVERSION_API } from "./shared/constants";
+
+  fetch(CONVERSION_API);
 </script>
 
 <Router>
