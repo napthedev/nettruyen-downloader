0cf4f36da5d8b3dd159c44205421ce9f160a842f
napthedev <phongna.dev@gmail.com>
2022-04-17 22:50:53 +0700

    Chore: change proxy url

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = "http://www.nettruyenmoi.com";
 export const API_REGEX =
   /^https?:\/\/(www)?\.?nettruyen.+\.((com)|(net))\/truyen-tranh\/.+$/gm;
-export const PROXY_URL = "https://nct.napdev.workers.dev";
+export const PROXY_URL = "https://proxy.naptest.workers.dev";
 export const CONVERSION_API = "https://urls-to-pdf.herokuapp.com";
